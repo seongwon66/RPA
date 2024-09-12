bde657069b66377703d7d41a578a9ac02956c38a
seongwon66 <swlee3808@gmail.com>
2024-09-12 16:29:45 +0900

    2주차실습

---
+++ 2week/ex0.py
@@ -0,0 +1,14 @@
+num= int(input("입력하시오"))
+print(num)
+num1 = num % 2
+
+if num1 == 0:
+    print("짝수입니다-true")
+elif num1 !=0:
+    print("홀수입니다-flase")
+
+
+#num = (input("입력하시오"))
+#print(num)
+#result = (num%2) ==0
+#print('짝수이다-result)
